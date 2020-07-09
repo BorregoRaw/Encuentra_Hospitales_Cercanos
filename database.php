9c6a4f580e170a03199e1cbc89b6fafc4cc1eee1
BorregoRaw <jesusordonez997@gmail.com>
2020-07-09 00:48:30 -0500

    Primer Commit

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+
+$server = 'localhost';
+$username = 'id14287988_jesus';
+$password = 'CO=CyV5W-Kk<=9U-';
+$database = 'id14287988_php_login';
+
+try {
+  $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+} catch (PDOException $e) { 
+  die('Conexion fallida: ' . $e->getMessage());
+}
+
+?>
---
+++ partials/header.php
@@ -0,0 +1,3 @@
+<header>
+  <a href="index.php">Encuentra Hospitales Cercanos </a>
+</header>
