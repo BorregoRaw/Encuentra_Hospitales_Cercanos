13045c2425b987d27f37d0af8bc817bfa693a7a6
BorregoRaw <jesusordonez997@gmail.com>
2020-07-10 18:13:10 -0500

    agregue tablas, en nueva vista

--- public_html/lista.php
+++ public_html/lista.php
@@ -45,29 +45,9 @@
     <?php endif; ?>
       
       
-      <h2>Para que es esta pagina</h2>
-
-      <div style="color: #000; font-style: italic">
-
-        <p>Esta pagina esta consebida para conocer los hospitales cercanos ademas de el cupo total, de los hospitales, esto con fin de evitar visitar lugares donde se anteinde infectados por COVID 19</p>
-
-        <p>-</p><p>-</p><p>-</p>
-        
-
-        <p>Ademas de que cada hospital pude mostrar los servicios que puede ofrecer y para los que esta equipado, esto basado en el sistema de atencion TRIAGE por el Instituto Mexicano del Seguro Social</p>
-
-      </div>
-
-      <img src="img/semaforo.jpg">
-
-      <div style="color: #000; font-style: italic">
-
-        <p>Se puede observar que existen diferentes tipos de atencion, dependiendo de la gravedad y situacion en la que se encuentre el solicitante del servicio, es por ello que cada hospital o centro de atencion medica puede ofrecer distinta atencion.</p>
-
-      </div>      
-      <h2><a href="lista.php"> Clic aqui para ver la lista de los hospitales registrados </a></h2>
-
-      <!--  
+      
+      <h2><a href="logout.php"> Salir </a></h2>
+      
       <div align='center'> 
         <table class="egt">
 
@@ -121,7 +101,7 @@
               <td>'.$results['tipoServicio'].' </td>
               <td>'.$results['direccion'].' </td>
               <td>'.$results['cupo'].' </td>   
-
+              
               <td>  <a href="https://www.google.com.mx/maps/place/Hospital+'.$results['nombre'].'+'.$results['direccion'].'"> Como llegar </a> </td>
               </tr>';
 
@@ -133,7 +113,7 @@
         ?>
 
         </table>
-        -->
+
       </div>
   </body>
 </html>
